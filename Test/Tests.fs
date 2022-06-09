514bf9d65c30fae71a3d64f0c2c5268dc685af87
Roly Perera <roly.perera@dynamicaspects.org>
2022-06-09 08:55:52 +0100

    🧩 [add-unused]: Capture stdout in noEnvVars test.

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -1,10 +1,15 @@
 module Tests
 
 open System
+open System.IO
 open Xunit
 open NowWhat.CLI
 
 [<Fact>]
-let ``My test`` () =
-    nowwhat ()
+let noEnvVars () =
+    printfn $"Current directory {Environment.CurrentDirectory}"
+    let writer = new StreamWriter("../../../expected/noEnvVars.new.txt")
+    Console.SetOut(writer)
+    nowwhat () |> ignore
+    writer.Flush()
     Assert.True(true)
