e803e4ad48e2faeab6359b576b1e78d4206e4a8a
= <jgeddes@turing.ac.uk>
2022-06-14 15:19:35 +0100

    Get credentials from config files if environment vars not set

--- NowWhat/CLI.fs
+++ NowWhat/CLI.fs
@@ -1,43 +1,22 @@
 module NowWhat.CLI
 
 open NowWhat.API
-
-let envVars = {|
-  gitHub = "NOWWHAT_GITHUB_TOKEN";
-  forecastId = "FORECAST_ID";
-  forecastToken = "NOWWHAT_FORECAST_TOKEN"
-|}
-
-let gitHubVars = [envVars.gitHub]
-let forecastVars = [envVars.forecastId; envVars.forecastToken]
-
-let checkEnvironmentVariables (envVars: string List) =
-  let env = System.Environment.GetEnvironmentVariables()
-
-  (true, envVars)
-  ||> List.fold (fun allExist variable ->
-    if not (env.Contains variable) then
-      printfn $"Please set environment variable {variable}."
-      false
-    else
-      allExist)
+open NowWhat.Config
 
 let nowwhat argv =
     printfn "Now what?"
 
-    if checkEnvironmentVariables gitHubVars then
-      let gitHubToken = System.Environment.GetEnvironmentVariable(envVars.gitHub)
-      let githubIssues =
+    let secrets = getSecrets ()
+    printfn $"Secrets: {secrets}"
+
+    let githubIssues =
         Github.ProjectBoard
-        |> Github.getAllProjectIssues gitHubToken
+        |> Github.getAllProjectIssues secrets.githubToken
         |> snd
         |> Array.filter (fun (_, _, status, _) -> status = "OPEN")
-      printfn "Number of open issues in Project tracker: %d" githubIssues.Length
+    printfn "Number of open issues in Project tracker: %d" githubIssues.Length
 
-    if checkEnvironmentVariables forecastVars then
-      let forecastId = System.Environment.GetEnvironmentVariable(envVars.forecastId)
-      let forecastToken = System.Environment.GetEnvironmentVariable(envVars.forecastToken)
-      let forecastProjects = Forecast.getProjects forecastId forecastToken
-      printfn "Number of projects in Forecast: %d" (forecastProjects.Projects |> Seq.length)
+    let forecastProjects = Forecast.getProjects secrets.forecastId secrets.forecastToken
+    printfn "Number of projects in Forecast: %d" (forecastProjects.Projects |> Seq.length)
 
     0
