e1be27768640182c372ce9287c74401803fa9a16
Roly Perera <roly.perera@dynamicaspects.org>
2022-06-15 11:34:28 +0100

    🧩 [refactor]: Extract fixtureDir.

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -7,11 +7,12 @@ open NowWhat.CLI
 open NowWhat.DomainModel
 open Thoth.Json.Net
 
+let fixtureDir: string = $"{__SOURCE_DIRECTORY__}/fixtures"
 
 type RedirectStdOut(fileNameStub: string) =
     // Store native stdout for later use
     let consoleStdOut = Console.Out
-    let capturedStdOut = new StreamWriter($"{__SOURCE_DIRECTORY__}/fixtures/{fileNameStub}.new.txt")
+    let capturedStdOut = new StreamWriter($"{fixtureDir}/{fileNameStub}.new.txt")
 
     // Setup: redirect output to a StringWriter
     do Console.SetOut(capturedStdOut)
@@ -23,8 +24,8 @@ type RedirectStdOut(fileNameStub: string) =
             Console.SetOut(consoleStdOut)
 
 let stdOutMatches (fileNameStub: string) =
-    let expectedPath = $"{__SOURCE_DIRECTORY__}/fixtures/{fileNameStub}.txt"
-    let actualPath = $"{__SOURCE_DIRECTORY__}/fixtures/{fileNameStub}.new.txt"
+    let expectedPath = $"{fixtureDir}/{fileNameStub}.txt"
+    let actualPath = $"{fixtureDir}/{fileNameStub}.new.txt"
     Assert.Equal(File.ReadAllText(expectedPath), File.ReadAllText(actualPath))
     // We will only delete the file and return 'true' if the previous Assert succeeded
     File.Delete(actualPath)
@@ -42,7 +43,7 @@ let ``End-to-end test with environment variables`` (fileNameStub: string) =
 [<InlineData("rootSerialised.json")>]
 let ``test Forecast JSON deserialisation`` (jsonFileName: string) =
     let expected =  { ForecastModel.Root.projects = [{ id = 1684536; name = "Time Off"; color = "black"; code = None; notes = None }] }
-    let rootJson = File.ReadAllText($"{__SOURCE_DIRECTORY__}/fixtures/{jsonFileName}")
+    let rootJson = File.ReadAllText($"{fixtureDir}/{jsonFileName}")
     let actual = match rootJson |> Decode.fromString ForecastModel.rootDecoder with
                  | Ok projects -> projects
                  | Error _ -> { ForecastModel.Root.projects = [] }
